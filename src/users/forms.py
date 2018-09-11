4c8c4b78bd21d2647fceb52b1a116c6bf5b97073
chairco <chairco@gmail.com>
2018-09-11 11:37:14 +0800

    Update register template

--- src/users/forms.py
+++ src/users/forms.py
@@ -20,5 +20,5 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
+            raise forms.ValidationError("Passwords don\'t match.")
         return cd['password2']
